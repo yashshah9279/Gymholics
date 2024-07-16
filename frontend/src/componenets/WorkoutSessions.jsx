f065b80f421973a8eb5b309c88e6178e532d0efe
Yash <yashshah@gmail.com>
2024-07-16 21:08:40 +0530

    info changes

--- frontend/src/componenets/WorkoutSessions.jsx
+++ frontend/src/componenets/WorkoutSessions.jsx
@@ -5,27 +5,44 @@ const WorkoutSessions = () => {
      <section className='workout_session'>
         <div className="wrapper">
             <h1>Top Workout Session</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi sed quia commodi similique obcaecati id recusandae quis itaque!</p>
+            <p>
+            The top workout session combines strength training, cardio, and flexibility exercises to maximize fitness, improve endurance, and enhance overall health</p>
             <img src="/img5.jpg" alt="workout" />
         </div>
         <div className="wrapper">
             <h1>Featured bootcamps</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea, quaerat quod! Aspernatur, molestiae. Sit laboriosam similique eius odio.</p>
+            <p>Featured boot camps offer intensive, goal-driven fitness programs tailored to diverse fitness levels. With expert guidance, varied training styles like HIIT and strength training, and supportive community environments, participants achieve measurable results while enjoying state-of-the-art facilities and nutritional support.</p>
             <div className="bootcamp">
-                <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, consectetur!</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt sapiente cumque magnam itaque praesentium quis.</p> 
+                <h4>Intense Workouts for Results:</h4>
+                <p>Our featured bootcamps offer intense, results-driven workouts tailored to all fitness levels. Whether you're a beginner or a seasoned athlete, these programs deliver exceptional fitness gains.</p> 
             </div>
             <div className="bootcamp">
-                <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, consectetur!</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt sapiente cumque magnam itaque praesentium quis.</p> 
+                <h4>Expert Guidance and Support:</h4>
+                <p>Join our featured bootcamps and benefit from expert guidance and unwavering support. Our certified trainers ensure you stay motivated and achieve your fitness goals effectively.</p> 
             </div>
             <div className="bootcamp">
-                <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, consectetur!</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt sapiente cumque magnam itaque praesentium quis.</p> 
+                <h4>Variety of Training Styles</h4>
+                <p>Experience a variety of training styles in our featured bootcamps, from high-intensity interval training (HIIT) to strength and conditioning. Discover what works best for your body and fitness aspirations.</p> 
             </div>
             <div className="bootcamp">
-                <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, consectetur!</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt sapiente cumque magnam itaque praesentium quis.</p> 
+                <h4>Community and Camaraderie:</h4>
+                <p>Foster a sense of community and camaraderie in our featured bootcamps. Connect with like-minded individuals who share your passion for health and fitness, making workouts enjoyable and rewarding.</p> 
+            </div>
+            <div className="bootcamp">
+                <h4>Flexible Scheduling Options:</h4>
+                <p>Enjoy flexible scheduling options with our featured bootcamps, designed to fit seamlessly into your busy lifestyle. Whether early morning or evening sessions, we accommodate your preferences for consistent progress.</p> 
+            </div>
+            <div className="bootcamp">
+                <h4>Nutritional Guidance: </h4>
+                <p>Receive comprehensive nutritional guidance in our featured bootcamps to complement your fitness journey. Learn how to fuel your body for optimal performance and results.</p> 
+            </div>
+            <div className="bootcamp">
+                <h4>Goal-Oriented Programs:</h4>
+                <p>Our featured bootcamps are structured around specific goals, such as weight loss, muscle gain, or overall fitness improvement. Achieve measurable results with targeted programs.</p> 
+            </div>
+            <div className="bootcamp">
+                <h4>Progress Tracking:</h4>
+                <p> Benefit from progress tracking and assessments in our featured bootcamps. Monitor your improvements over time and adjust your workouts for continuous advancement.</p> 
             </div>
         </div>
      </section>
